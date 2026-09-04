fce357f367897857cb621dffdf5d4614a010a2d9
bhuvangolhar <bhuvangolhar2025@gmail.com>
2026-09-04 23:58:37 +0530

    Create a program in Java to print "Hello World"

---
+++ Java/000.java
@@ -0,0 +1,11 @@
+// Java program: Hello World
+
+package projectFolder
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
