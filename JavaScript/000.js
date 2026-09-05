f417e6b4ceec08e70cf59fb25d118dcde20b8575
bhuvangolhar <bhuvangolhar2025@gmail.com>
2026-09-05 23:58:29 +0530

    Create a JavaScript file to enhance "Hello World"

---
+++ JavaScript/000.js
@@ -0,0 +1,27 @@
+<!-- HTML file: Hello World -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+    <link rel="stylesheet" href="000.css">
+</head>
+<body>
+    <p id="heading"></p>
+    <script src="000.js"></script>
+</body>
+</html>
+
+
+/* CSS file: Hello World */
+
+body {
+    background-color: white;
+}
+
+
+// JavaScript file: Hello World
+
+document.getElementById("heading").innerHTML = "Hello World";
